d33a14bde18ef3618418b33e169bf7ce54fabdda
Markus Kohn <post@markus-kohn.com>
2026-04-21 14:54:10 +0200

    Add Klingon, Plattdeutsch, Saxon locale and validation files

---
+++ lang/sxu/validation.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'required' => 'Das Feld :attribute mussde ausfilln.',
+    'email' => 'Das Feld :attribute muss ne orndlsche Mähl-Adrässe sei.',
+    'unique' => ':attribute is schonn vorgeeem.',
+    'string' => 'Das Feld :attribute muss ne Zeischnkedde sei.',
+    'max' => [
+        'string' => 'Das Feld :attribute derf nisch länger als :max Zeischn sei.',
+    ],
+    'min' => [
+        'string' => 'Das Feld :attribute muss mindesdns :min Zeischn lang sei.',
+    ],
+    'in' => 'Der gewählde Werrd fer :attribute is ungildsch.',
+    'attributes' => [
+        'locale' => 'Sbrache',
+    ],
+];
---
+++ lang/tlh/validation.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'required' => 'yab :attribute poQlu\'.',
+    'email' => 'yab :attribute De\'wI\' ngeb lu\'lu\'bogh moj.',
+    'unique' => ':attribute Sampu\'.',
+    'string' => 'yab :attribute mu\'tlhegh moj.',
+    'max' => [
+        'string' => 'yab :attribute law\'be\' :max tetlh law\'.',
+    ],
+    'min' => [
+        'string' => 'yab :attribute loQ :min tetlh law\' moj.',
+    ],
+    'in' => ':attribute wIvlu\'pu\'bogh lu\'be\'lu\'.',
+    'attributes' => [
+        'locale' => 'Hol',
+    ],
+];
